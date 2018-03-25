b2e463a89b51f6a05b894a1a1112f639bad87eb4
ibs85 <35095745+ibs85@users.noreply.github.com>
2018-03-25 12:32:44 +0530

    Update and rename lineage.mk to fh.mk

--- fh.mk
+++ fh.mk
@@ -13,10 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit some common A6000 stuff.
 $(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FireHound stuff.
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -24,8 +25,11 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
 
-PRODUCT_NAME := lineage_a6000
 BOARD_VENDOR := Lenovo
+PRODUCT_BRAND := Lenovo
+PRODUCT_DEVICE := a6000
+PRODUCT_NAME := fh_a6000
+PRODUCT_MANUFACTURER := Lenovo
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
